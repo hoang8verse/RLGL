9030c246c657bf97884565dc63d5b41df7b58fce
thuannguyen2209 <thuan.nguyen@8verse.games>
2023-03-15 16:03:37 +0700

    Add basic scripts for UI elements management

---
+++ Assets/Scripts/UIScripts/JoinGameScreen.cs
@@ -0,0 +1,51 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIElements
+{
+    public class JoinGameScreen : Screen
+    {
+        [SerializeField] TMP_InputField m_inputField;
+        [SerializeField] TextMeshProUGUI m_notificationText;
+
+        [SerializeField]
+        private string m_roomIDEntered;
+        public string RoomIDEntered => m_roomIDEntered;
+        private TouchScreenKeyboard m_touchScreenKeyboard;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnShowNotificationMessage(string notificationText = "Mã phòng không tồn tại*")
+        {
+            m_notificationText.text = notificationText;
+            m_notificationText.gameObject.SetActive(true);
+        }
+        public void OnEnteredRoomID()
+        {
+            m_roomIDEntered = m_inputField.text;
+        }
+        public void OnInputFieldSelected()
+        {
+            m_touchScreenKeyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        }
+        public void OnJoinRoom()
+        {
+            
+        }
+        public void OnExitScreen()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/UIScripts/LobbyScreen.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using ZXing;
+using ZXing.QrCode;
+
+namespace UIElements
+{
+    public class LobbyScreen : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI m_roomID;
+        [SerializeField] RawImage m_qrImage;
+        [SerializeField] Dictionary<string, PlayerAvatar> m_playerAvatarsList;
+
+        public string RoomID => m_roomID.text;
+        public RawImage QRImage => m_qrImage;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_qrImage.texture = GetQRCodeTexture(m_roomID.text, 512, 512);
+        }
+
+        public void SetAvatarForPlayer(Texture2D avatarImage, string playerID)
+        {
+            m_playerAvatarsList[playerID].SetAvatarImage(avatarImage);
+        }
+        public void SetRoomID(string roomID)
+        {
+            m_roomID.text = roomID;
+        }
+        public void OnCopyRoomID()
+        {
+            
+        }
+        public void OnStartGame()
+        {
+        
+        }    
+
+        private Texture2D GetQRCodeTexture(string text, int width, int height)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width
+                }
+            };
+            var result = writer.Write(text);
+            var texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.SetPixels32(result);
+            texture.Apply();
+            return texture;
+        }
+    }
+}
