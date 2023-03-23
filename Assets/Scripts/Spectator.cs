756055d150cf507903cccc9372e1ab96284ff466
Vu Ngoc Hoang <hoang8verse@gmail.com>
2023-03-23 19:26:25 +0700

    - support move camera Spectator for mobile

--- Assets/Scripts/Spectator.cs
+++ Assets/Scripts/Spectator.cs
@@ -18,11 +18,28 @@ public class Spectator : MonoBehaviour
     AudioSource clicked;
     private Transform ResultTransfrom;
 
+    bool isMovingCamera = false;
+    bool isRunOnMobile = false;
     // Start is called before the first frame update
     void Start()
     {
         EndGameScreen.SetActive(false);
         GameTimer.text = "";
+        CheckDevice();
+    }
+
+    void CheckDevice()
+    {
+        if (Application.isMobilePlatform)
+        {
+            Debug.Log("Running on a mobile device.");
+            isRunOnMobile = true;
+        }
+        else
+        {
+            Debug.Log("Running on a non-mobile device.");
+            isRunOnMobile = false;
+        }
     }
 
     // Update is called once per frame
@@ -51,13 +68,100 @@ public class Spectator : MonoBehaviour
             camera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * 6;
         }
 
-        if(camera.fieldOfView < 60f)
+        if (isRunOnMobile)
         {
-            camera.fieldOfView = 60f;
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0); // trying to get the second touch input
+                //Debug.Log(" --------------------------------  spectator touch.position.x ------------------------------------  " + touch.position);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    isMovingCamera = true;
+                    Debug.Log(" ======================== GetTouch mobile  downnnnnn ========== =  ");
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    //Debug.Log(" ================= GetTouch mobile  movingggggggg ===========  ");
+                }
+                else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    isMovingCamera = false;
+
+                    Debug.Log(" ======================== GetTouch mobile  uppppppppp ===========  ");
+                }
+
+                if (isMovingCamera)
+                {
+                    //Debug.Log(" --------------------------------  spectator move camera------------------------------------  ");
+                    if (touch.position.x < 1080 / 2)
+                    {
+                        if (camera.transform.position.x > -120f)
+                            camera.transform.position = new Vector3(camera.transform.position.x - 2 * OFFSET_MOVE * Time.deltaTime, camera.transform.position.y, camera.transform.position.z);
+                    } 
+                    if (touch.position.x > 1080 / 2)
+                    {
+                        if (camera.transform.position.x < 120f)
+                            camera.transform.position = new Vector3(camera.transform.position.x + 2 * OFFSET_MOVE * Time.deltaTime, camera.transform.position.y, camera.transform.position.z);
+                    }
+
+                    if (touch.position.y < 1920 / 2)
+                    {
+                        camera.fieldOfView += OFFSET_MOVE * Time.deltaTime;
+                    }
+                    if (touch.position.y > 1920 / 2)
+                    {
+                        camera.fieldOfView -= OFFSET_MOVE * Time.deltaTime;
+                    }
+                }
+
+            }
+        }
+        //else
+        //{
+        //    if (Input.GetMouseButtonDown(0)) // 0 : left , 1 : right, 2 : wheel
+        //    {
+        //        isMovingCamera = true;
+        //    }
+        //    else if (Input.GetMouseButton(0)) // 0 : left , 1 : right, 2 : wheel
+        //    {
+
+        //    }
+        //    else if (Input.GetMouseButtonUp(0))
+        //    {
+        //        isMovingCamera = false;
+        //    }
+        //    if (isMovingCamera)
+        //    {
+        //        Debug.Log(" -------------------------------- PC spectator move camera------------------------------------  ");
+        //        if (Input.GetAxis("Mouse X") < 1080 / 2)
+        //        {
+        //            if (camera.transform.position.x > -120f)
+        //                camera.transform.position = new Vector3(camera.transform.position.x - 2 * OFFSET_MOVE * Time.deltaTime, camera.transform.position.y, camera.transform.position.z);
+        //        }
+        //        if (Input.GetAxis("Mouse X") > 1080 / 2)
+        //        {
+        //            if (camera.transform.position.x < 120f)
+        //                camera.transform.position = new Vector3(camera.transform.position.x + 2 * OFFSET_MOVE * Time.deltaTime, camera.transform.position.y, camera.transform.position.z);
+        //        }
+
+        //        if (Input.GetAxis("Mouse Y") < 1920 / 2)
+        //        {
+        //            camera.fieldOfView += OFFSET_MOVE * Time.deltaTime;
+        //        }
+        //        if (Input.GetAxis("Mouse Y") > 1920 / 2)
+        //        {
+        //            camera.fieldOfView -= OFFSET_MOVE * Time.deltaTime;
+        //        }
+        //    }
+        //}
+
+        if (camera.fieldOfView < 50f)
+        {
+            camera.fieldOfView = 50f;
         } 
-        if(camera.fieldOfView > 120f)
+        if(camera.fieldOfView > 150f)
         {
-            camera.fieldOfView = 120f;
+            camera.fieldOfView = 150f;
         }
     }
 
